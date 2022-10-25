abde32164464f0e7b4c5da1dfb0c7bdee4a3d7df
N4tht4N <n4tht4n@mailbox.org>
2022-10-25 21:52:23 +0200

    Add telescope neovim plugin

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -15,3 +15,9 @@ vim.g.mapleader = " "
 --   term mode:           "t"
 --   command mode:        "c"
 
+-- Telescope
+local builtin = require('telescope.builtin')
+keymap('n', '<leader>ff', builtin.find_files, {})
+keymap('n', '<leader>fg', builtin.live_grep, {})
+keymap('n', '<leader>fb', builtin.buffers, {})
+
