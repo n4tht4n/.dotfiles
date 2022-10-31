cd071e6824051c69f3c19d07694a42a0cfb6c02e
N4tht4N <n4tht4n@mailbox.org>
2022-10-31 10:47:45 +0100

    Re-add tmux... for now

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -41,7 +41,8 @@ require('packer').init({
 return require('packer').startup(function(use)
   use { 'wbthomason/packer.nvim' }
 
-  use { 'nvim-lua/plenary.nvim' } -- provides a lot of funcs for other plugins
+  use { 'christoomey/vim-tmux-navigator' }    -- required for perfect nvim <-> tmux integration
+  use { 'nvim-lua/plenary.nvim' }             -- provides a lot of funcs for other plugins
 
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.0' }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
--- zsh/my_scripts.zsh
+++ zsh/my_scripts.zsh
@@ -2,3 +2,28 @@
 
 # Add stuff, as needed... 😉
 
+### loads and shows all tmuxp configs to start a session ###
+ftmuxp() {
+  if [[ -n $TMUX ]]; then
+    return
+  fi
+
+  # using 'exa' would always yield the target of symlinks, need to get rid of that
+  ID="$(ls $XDG_CONFIG_HOME/tmuxp | cut -d ' ' -f1 | sed -e 's/\.yml$//')"
+  if [[ -z "$ID" ]]; then
+    tmux new-session
+  fi
+
+  create_new_session="Create New Session"
+
+  ID="${create_new_session}\n$ID"
+  ID="$(echo $ID | fzf | cut -d ' ' -f1)"
+
+  if [[ "$ID" = "${create_new_session}" ]]; then
+    tmux new-session
+  elif [[ -n "$ID" ]]; then
+    # TODO: check howto tell Kitty the session name!?
+    tmuxp load "$ID"
+  fi
+}
+
