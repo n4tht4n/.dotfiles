38893f15c900a0884b1b0b8aa3d0fe3f3d4b2d9a
N4tht4N <n4tht4n@mailbox.org>
2022-11-16 14:17:01 +0100

    Show filepath in lualine statusbar

--- nvim/lua/me/plugins/lualine.lua
+++ nvim/lua/me/plugins/lualine.lua
@@ -9,6 +9,14 @@ lualine.setup({
     globalstatus = true,
     theme = "dracula",
   },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {{'filename', path = 1}},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
   tabline = {
     lualine_a = {'buffers'},
     lualine_b = {},
