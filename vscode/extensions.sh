dc3f05025c3b4e53f09a9068367242a8ee9f175e
N4tht4N <n4tht4n@mailbox.org>
2022-11-24 16:51:28 +0100

    Add initial vscode settings

---
+++ vscode/extensions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Bootstraping vscode extensions..."
+
+code --install-extension editorconfig.editorconfig
+code --install-extension dracula-theme.theme-dracula
+code --install-extension esbenp.prettier-vscode
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension auiworks.amvim
